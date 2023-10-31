386b1a82bbe6b1f86dc29cec471e0c94223fa081
lesley <lesleybanadzem2017@gmail.com>
2023-10-30 22:50:15 -0500

    initial App config

---
+++ app/utils.test.ts
@@ -0,0 +1,12 @@
+const validateEmail = (str: string | null | undefined) => {};
+test("validateEmail returns false for non-emails", () => {
+  expect(validateEmail(undefined)).toBe(false);
+  expect(validateEmail(null)).toBe(false);
+  expect(validateEmail("")).toBe(false);
+  expect(validateEmail("not-an-email")).toBe(false);
+  expect(validateEmail("n@")).toBe(false);
+});
+
+test("validateEmail returns true for emails", () => {
+  expect(validateEmail("kody@example.com")).toBe(true);
+});
