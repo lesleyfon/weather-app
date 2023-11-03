b84df2c329f236abcd7a0f080bd5f55eb63e8b88
lesley <lesleybanadzem2017@gmail.com>
2023-11-02 22:00:15 -0500

    Created a few new utility functions

--- app/utils.ts
+++ app/utils.ts
@@ -1,93 +1,49 @@
-import { AutocompleteResponse, ResultsType } from "./types/location.types";
-import dayjs from "dayjs";
-import type { ENV_TYPES } from "./types/location.types";
+import type {
+  LineGraphReturnedData,
+  WeatherData,
+} from "./types/location.types";
 
-const requestOptions: RequestInit = {
-  method: "GET",
-  redirect: "follow",
-  mode: "cors",
-  referrerPolicy: "strict-origin-when-cross-origin",
-  headers: {
-    accept: "*/*",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"macOS"',
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "cross-site",
-  },
+export const militaryTimeToStandardTime = (time: string) => {
+  const [hours] = time.split(":");
+  const h = parseInt(hours);
+  const ampm = h >= 12 ? "pm" : "am";
+  const hour = h % 12 || 12;
+
+  const standardTime = hour + " " + ampm;
+  return standardTime;
 };
 
-export async function getAutocompletePrediction({
-  input,
-  GOOGLE_API_KEY,
-}: {
-  input: string;
-  GOOGLE_API_KEY: ENV_TYPES["GOOGLE_API_KEY"];
-}): Promise<AutocompleteResponse | null> {
-  try {
-    const encodedInput = encodeURI(input);
+export const dateStringTo = (date: string) =>
+  new Date(date).toLocaleDateString("en-US", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
-    const response = await fetch(
-      `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${encodedInput}&key=${GOOGLE_API_KEY}`,
-      requestOptions,
-    );
-    const data: AutocompleteResponse = await response.json();
+export const celsiusToFahrenheit = (temp: number) => {
+  const t = ((temp * 9) / 5 + 32).toFixed(2);
+  return t;
+};
 
-    return data;
-  } catch (error) {
-    console.log("error", error);
-    return null;
-  }
-}
+export const formatDataToGraph = (data: WeatherData) => {
+  if (!data?.days[0]) return [];
+  const { hours, tempmax, tempmin } = data?.days[0];
 
-export async function getGeocode({
-  input,
-  GOOGLE_API_KEY,
-}: {
-  input: string;
-  GOOGLE_API_KEY: ENV_TYPES["GOOGLE_API_KEY"];
-}) {
-  try {
-    const encodedInput = encodeURI(input);
+  const d = hours.map((hour): LineGraphReturnedData => {
+    const { temp, datetime, windspeed, precip, humidity } = hour;
+    const time = militaryTimeToStandardTime(datetime.slice(0, -3));
 
-    const response = await fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedInput}&key=${GOOGLE_API_KEY}`,
-      requestOptions,
-    );
-
-    const data: ResultsType = await response.json();
-
-    return data;
-  } catch (error) {}
-}
-
-export async function getWeatherLocation({
-  longitude,
-  latitude,
-  date,
-  WEATHER_VISUAL_CROSSING_API_KEY,
-}: {
-  longitude: string;
-  latitude: string;
-  date: string;
-  WEATHER_VISUAL_CROSSING_API_KEY: ENV_TYPES["WEATHER_VISUAL_CROSSING_API_KEY"];
-}) {
-  try {
-    const requestOptions: RequestInit = {
-      method: "GET",
-      redirect: "follow",
+    return {
+      name: time,
+      temp: celsiusToFahrenheit(temp),
+      tempmax: celsiusToFahrenheit(tempmax),
+      tempmin: celsiusToFahrenheit(tempmin),
+      precipitation: precip,
+      humidity,
+      windspeed,
     };
-    const formattedDate = dayjs(date).format("YYYY-MM-DD");
+  });
 
-    const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${latitude},${longitude}/${formattedDate}?unitGroup=metric&key=${WEATHER_VISUAL_CROSSING_API_KEY}`,
-      requestOptions,
-    );
-
-    const data = await response.text();
-
-    return data;
-  } catch (error) {
-    console.log("error", error);
-  }
-}
+  return d;
+};
