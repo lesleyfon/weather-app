d9ddda7971dfeaa0b58e0da2a15d00195f0be9a7
lesley <lesleybanadzem2017@gmail.com>
2025-01-06 20:54:59 -0800

    Enhance weather comparison form with improved UI and error handling
    - Added a Header component for better navigation and branding.
    - Updated error messages to provide clearer guidance for user input.
    - Enhanced styling of error messages for better visibility.
    - Reorganized the main layout to include a title and description for the weather comparison feature.
    - Maintained existing functionality while improving overall user experience.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,13 @@
 import { json, redirect, type ActionFunctionArgs } from "@remix-run/node";
-import { Form, useActionData, useSearchParams } from "@remix-run/react";
+import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
 import { useMemo } from "react";
 
 import { getGeocode } from "~/api/weather.api";
+import { SVG } from "~/components/chartcustomizedcursor";
 import DatePickerComponent from "~/components/datepicker";
 import LocationAutoComplete from "~/components/locationautocomplete";
 import { Button } from "~/components/ui/button";
+import { Header } from "~/components/ui/header";
 import { ENV_TYPES, QUERY_PARAMS_ENUM, DateType } from "~/types/location.types";
 import { setURLParamsAsDefaultDate } from "~/utils";
 
@@ -29,7 +31,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
     errors.location = "Location is required";
   }
   if (!firstDate || !secondDate) {
-    errors.date = "Date is required";
+    errors.date = "Both dates are required";
   }
 
   if (Object.keys(errors).length > 0) {
@@ -62,7 +64,7 @@ function FormError({
 }) {
   if (errors && errors.location && errors.date) {
     return (
-      <p className="tw-text-left tw-w-full tw-text-red-500">
+      <p className="tw-text-left tw-w-full tw-text-red-700  tw-px-4">
         You are missing both Location and Date. Please Ensure you Include a
         location and a Date
       </p>
@@ -71,12 +73,14 @@ function FormError({
 
   if (errors && errors.date) {
     return (
-      <p className="tw-text-left tw-w-full tw-text-red-500">{errors.date}</p>
+      <p className="tw-text-left tw-w-full tw-text-red-700 tw-px-4">
+        {errors.date}
+      </p>
     );
   }
   if (errors && errors.location) {
     return (
-      <p className="tw-text-left tw-w-full tw-text-red-500">
+      <p className="tw-text-left tw-w-full tw-text-red-700">
         {errors.location}
       </p>
     );
@@ -96,30 +100,46 @@ function Index() {
   );
 
   return (
-    <main className="tw-container tw-flex tw-flex-col tw-mt-7 md:tw-mt-36  tw-m-auto tw-items-center tw-h-screen tw-max-w-6xl tw-w-full">
-      <Form
-        method="post"
-        className="tw-grid tw-grid-rows-[repeat(4_1fr)] tw-grid-cols-[repeat(1,100%)]  md:tw-grid-rows-[1] md:tw-grid-cols-[1fr_1fr_1fr_82px] tw-gap-2 tw-justify-center tw-items-center tw-w-full tw-px-4"
-      >
-        <LocationAutoComplete />
-
-        <DatePickerComponent
-          dateType={DateType.FIRST}
-          defaultDate={defaultFirstDate}
-        />
-        <DatePickerComponent
-          dateType={DateType.SECOND}
-          defaultDate={defaultSecondDate}
-        />
-        <Button
-          type="submit"
-          className="tw-bg-indigo-950 tw-text-white tw-h-[52px]"
+    <>
+      <Header
+        heading={
+          <Link to="/" className="tw-flex tw-items-center tw-gap-2">
+            <SVG />
+            <span className="tw-text-indigo-900">Weather Compare</span>
+          </Link>
+        }
+      />
+      <main className="tw-container tw-flex tw-flex-col tw-mt-7 md:tw-mt-36  tw-m-auto tw-items-center tw-h-screen tw-max-w-6xl tw-w-full">
+        <h1 className="tw-text-center tw-text-2xl tw-font-bold tw-text-indigo-900">
+          Compare Weather Between Two Dates
+        </h1>
+        <p className="tw-text-center tw-text-lg tw-text-indigo-900 tw-my-4 ">
+          Enter a location and two dates to compare the weather between them.
+        </p>
+        <Form
+          method="post"
+          className="tw-grid tw-grid-rows-[repeat(4_1fr)] tw-grid-cols-[repeat(1,100%)]  md:tw-grid-rows-[1] md:tw-grid-cols-[1fr_1fr_1fr_82px] tw-gap-2 tw-justify-center tw-items-center tw-w-full tw-px-4"
         >
-          Submit
-        </Button>
-      </Form>
-      {actionData?.errors ? <FormError errors={actionData.errors} /> : null}
-    </main>
+          <LocationAutoComplete />
+
+          <DatePickerComponent
+            dateType={DateType.FIRST}
+            defaultDate={defaultFirstDate}
+          />
+          <DatePickerComponent
+            dateType={DateType.SECOND}
+            defaultDate={defaultSecondDate}
+          />
+          <Button
+            type="submit"
+            className="tw-bg-indigo-950 tw-text-white tw-h-[52px]"
+          >
+            Submit
+          </Button>
+        </Form>
+        {actionData?.errors ? <FormError errors={actionData.errors} /> : null}
+      </main>
+    </>
   );
 }
 
